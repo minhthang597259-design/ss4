34617620fd8a8c4ee9c552ae6aa6ee64f25b1427
minhthang597259-design <minhthang597259@gmail.com>
2026-04-23 11:04:55 +0700

    Add files via upload

---
+++ b3.java
@@ -0,0 +1,13 @@
+package btvnss1;
+import java.util.Scanner;
+public class b3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
+        System.out.println(((a*d)+(b*c)) +"/"+ (b*d));
+    }
+}
