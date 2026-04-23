8a3e96e1432ddd2958c6a7318a154d382b99b464
minhthang597259-design <minhthang597259@gmail.com>
2026-04-23 11:05:56 +0700

    Update b1.java

--- b1.java
+++ b1.java
@@ -1,10 +1,34 @@
-package btvnss1;
 import java.util.Scanner;
-public class b1 {
+
+public class BubbleSortDescending {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("nhap ban kinh : ");
-        double r = sc.nextDouble();
-        System.out.printf("dien tich : %.2f\n", Math.PI * r * r);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhap kich thuoc cua mang: ");
+        int n = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap gia tri cho phan tu thu " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
+        System.out.println("Mang da sap xep theo thu tu giam dan:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+        scanner.close();
     }
 }
