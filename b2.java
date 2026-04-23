ee9709068915851d7f95e545922b61c0c635184f
minhthang597259-design <minhthang597259@gmail.com>
2026-04-23 11:07:21 +0700

    Update b2.java

--- b2.java
+++ b2.java
@@ -1,17 +1,40 @@
-package btvnss1;
 import java.util.Scanner;
-public class b2 {
+
+public class SumEvenOdd2DArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        System.out.println("first : "+a);
-        System.out.println("second : "+b);
-        System.out.println("tong : "+ (a+b));
-        System.out.println("hieu : "+ (a-b));
-        System.out.println("tich : "+ (a*b));
-        System.out.println("thuong : "+ (a/b));
-        System.out.println("phan du : "+ (a%b));
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhap so hang cua mang: ");
+        int rows = scanner.nextInt();
+        System.out.print("Nhap so cot cua mang: ");
+        int cols = scanner.nextInt();
+        
+        int[][] arr = new int[rows][cols];
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Nhap gia tri cho phan tu o vi tri [" + i + "][" + j + "]: ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        
+        int sumEven = 0;
+        int sumOdd = 0;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (arr[i][j] % 2 == 0) {
+                    sumEven += arr[i][j];
+                } else {
+                    sumOdd += arr[i][j];
+                }
+            }
+        }
+        
+        System.out.println("\n--- Ket Qua ---");
+        System.out.println("Tong cac so chan: " + sumEven);
+        System.out.println("Tong cac so le: " + sumOdd);
+        
+        scanner.close();
     }
 }
