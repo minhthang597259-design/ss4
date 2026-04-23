09b10392d6554396b9ae61080d4c84f316fcc984
minhthang597259-design <minhthang597259@gmail.com>
2026-04-23 11:09:17 +0700

    Update b4.java

--- b4.java
+++ b4.java
@@ -1,15 +1,49 @@
-package btvnss1;
 import java.util.Scanner;
-public class b4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a,b,c,d;
-        System.out.println("chieu dai : ");
-        a=sc.nextInt();
-        System.out.println("chieu rong : ");
-        b=sc.nextInt();
-        System.out.println("dien tich : " + (a*b));
-        System.out.println("chu vi : " + (a+b));
 
+public class EvenOddSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhap so phan tu cua mang: ");
+        int n = scanner.nextInt();
+        
+        if (n <= 0) {
+            System.out.println("Mảng không có phần tử");
+            scanner.close();
+            return;
+        }
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap phan tu thu " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        
+        int[] temp = new int[n];
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                temp[index++] = arr[i];
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                temp[index++] = arr[i];
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+        
+        System.out.println("Mang sau khi sap xep:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        
+        scanner.close();
     }
 }
